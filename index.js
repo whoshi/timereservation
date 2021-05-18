3fa4a4f861e5cf19642481adb9af4e6045571cdf
whoshi <whoshi@v004.vaio.ne.jp>
2021-05-19 00:14:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ $(function ()
     {
       
       
-      const resultrsvdateid = document.getElementById("resultdate").value;//
+      const resultrsvdateid = document.getElementById("resultdate");//
       var mes = "出庫予約を受け付けました。\n予定の10分前にLINEでお知らせします。\n予定時刻に間に合うように\n駐車場にお越しください。";
       const date1 = new Date();
       const date2 = date1.getFullYear() + "年" + 
@@ -31,7 +31,7 @@ $(function ()
        var mes2 = "\n----------------------------------------\n";
        var mes3 = "【予約内容】\n受付NO.NSPARK-2021NC-294\n";
        var mes4 = "受付日時: " + date2 + "\n入庫NO: 8\n"
-       var mes5 = "出庫予約時間: " + resultrsvdateid;
+       var mes5 = "出庫予約時間: " + resultrsvdateid.innerText;
        alert(mes + mes2 + mes3 + mes4 + mes5);
        
        
