06c0f59e94fe4286b85d183510b82192d1390843
whoshi <whoshi@v004.vaio.ne.jp>
2021-05-18 23:11:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(function () 
 {
     // カレンダー
-    /*	
+    	
     $(function () {
 	$.datepicker.setDefaults($.datepicker.regional["ja"]);    
         $('input[name="date"]').datepicker({
@@ -11,7 +11,7 @@ $(function ()
 	
 	
     });
-   */
+   
     
     // 送信
     $('form').submit(function () 
