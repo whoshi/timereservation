d43fb85b306bded80795029423cf13b6329fc877
whoshi <whoshi@v004.vaio.ne.jp>
2021-05-19 23:11:18 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -48,7 +48,7 @@ function sendMessages(text) {
 function shareTargetPicker(text) {
     liff.shareTargetPicker([{
         'type': 'text',
-        'text': text
+        '': text
     }]).catch(function (error) {
         window.alert('Failed to send message ' + error);
     });
